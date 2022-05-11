2e5c07d8bd303f856ea51aac3d279c12a91dc59a
Nick <handnf@gmail.com>
2022-05-10 21:33:08 -0400

    Initial tool development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatTabsModule} from '@angular/material/tabs';
+import { WikitionaryComponent } from './tools/wikitionary/wikitionary.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WikitionaryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tools/wikitionary/wikitionary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tool-wikitionary',
+  templateUrl: './wikitionary.component.html',
+  styleUrls: ['./wikitionary.component.css']
+})
+export class WikitionaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
