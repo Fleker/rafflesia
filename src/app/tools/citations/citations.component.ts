afb9ded527511291e0e018d7c9a31703c6551a7d
Nick Felker <handnf@gmail.com>
2022-06-17 20:19:37 -0400

    Refactor to enhance IEEE support

--- src/app/tools/citations/citations.component.ts
+++ src/app/tools/citations/citations.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpsCallableResult } from '@firebase/functions';
 import { Event } from 'src/app/event';
+import { FirebaseService } from 'src/app/firebase.service';
 
 interface CitationMisc {
   id: string
@@ -19,6 +21,7 @@ function getId(articleUrl: string) {
   const arr = articleUrl.split('/')
   const lastPath = arr[arr.length - 1]
   return lastPath.substring(0, 24) // Have some upperbound to size
+    || articleUrl.substring(0, 24) // Default to just the base URL
 }
 
 function getTitles(html: string) {
@@ -113,7 +116,7 @@ export class CitationsComponent implements OnInit {
   bibtex?: string
   mainbib = ['main.bib']
 
-  constructor() {}
+  constructor(private firebase: FirebaseService) {}
 
   ngOnInit(): void {
     window.addEventListener('message', e => {
@@ -136,21 +139,62 @@ export class CitationsComponent implements OnInit {
     window.parent.postMessage(msg, '*')
   }
 
+  /**
+   * This function may add special logic when citing specific websites.
+   * @param url URL of the document to reference
+   */
+  async linkCitePreprocessor(url: string) {
+    if (url.includes('ieeexplore.ieee.org/document')) {
+      const fnc = await this.firebase.getFunction('proxy')
+      const res = await fnc({url}) as HttpsCallableResult<string>
+      const html = await res.data
+      // <a _ngcontent-vov-c185="" append-to-href="?src=document" target="_blank" href="https://doi.org/10.1109/TSG.2021.3124490">10.1109/TSG.2021.3124490</a>
+      const doi = html.match(/https:\/\/doi.org\/([A-Za-z0-9./]+)/)![1] // Assume
+      console.log(`Contacting DOI with ${doi}`)
+      // Use custom format URL instead.
+      // Use same impl as in-browser fetch.
+      /**
+       * @ARTICLE{6598993,  author={},  journal={IEEE Vision for Smart Grid Control: 2030 and Beyond Reference Model},   title={IEEE Vision for Smart Grid Controls: 2030 and Beyond Reference Model},   year={2013},  volume={},  number={},  pages={1-10},  doi={10.1109/IEEESTD.2013.6598993}}
+       */
+      // https://github.com/davidagraf/doi2bib2/blob/master/server/doi2bib.js#L7
+      const citationFetch = await fetch(`https://doi.org/${doi}`, {
+        headers: {
+          'Accept': 'application/x-bibtex; charset=utf-8'
+        }
+      })
+      const bibtex = await citationFetch.text()
+      return bibtex // This is already in BibTeX
+    }
+
+    // Default impl -- Try reading site directly
+    const fnc = await this.firebase.getFunction('proxy')
+    const res = await fnc({url}) as HttpsCallableResult<string>
+    const html = await res.data
+    const titles = getTitles(html)
+    const citation: CitationMisc = {
+      id: getId(url),
+      author: getAuthors(html),
+      title: titles.title,
+      publisher: titles.publisher,
+      url,
+    }
+    return citationToBibtex(citation)
+  }
+
   async import() {
     if (!this.articleUrl) return // Nothing
     try {
-      const res = await fetch(`https://cors-anywhere.herokuapp.com/${this.articleUrl.trim()}`)
-      const html = await res.text()
-      const titles = getTitles(html)
-      const citation: CitationMisc = {
-        id: getId(this.articleUrl),
-        author: getAuthors(html),
-        title: titles.title,
-        publisher: titles.publisher,
-        url: this.articleUrl,
-      }
-      this.bibtex = citationToBibtex(citation)
+      const processedCitation = await this.linkCitePreprocessor(this.articleUrl)
+      this.bibtex = processedCitation
     } catch (e) {
+      const msg: Event = {
+        type: 'rafflesia_alert',
+        data: {
+          title: 'Cannot parse URL',
+          body: e
+        },
+      }
+      window.parent.postMessage(msg, '*')
       console.error(e)
     }
   }
